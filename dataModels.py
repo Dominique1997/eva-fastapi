b3e02d5c8b779bd3dd2f70a2295d67252fb95ec0
Dominique1997 <dominiqueliebens@telenet.be>
2023-11-05 18:55:56 +0100

    Creation of datamodels with update of cors

--- dataModels.py
+++ dataModels.py
@@ -8,6 +8,83 @@ class User(BaseModel):
 class Token(BaseModel):
     token: str
 
-class Login_query(BaseModel):
+class LoginQuery(BaseModel):
     login_query: str
 
+class CheckQuery(LoginQuery):
+    sentence: str
+    program_type: str
+
+class NewUser(LoginQuery):
+    username: str
+    password: str
+class UpdateUserGeneral(LoginQuery):
+    userid: int
+    new_username: str
+    new_password: str
+class UpdateUserName(LoginQuery):
+    userid: int
+    new_username: str
+
+class UpdateUserPassword(LoginQuery):
+    userid: int
+    new_password: str
+
+class DeleteUser(LoginQuery):
+    userid: int
+
+class SelectUser(LoginQuery):
+    program_type: str
+    username: str
+    password: str
+    discord_name: str
+
+class AddToken(LoginQuery):
+    userID: int
+    server_ip: str
+    server_port: str
+    server_username: str
+    server_password: str
+    server_token: str
+
+class UpdateTokenGeneral(LoginQuery):
+    tokenID: int
+    userID: int
+    server_ip: str
+    server_port: str
+    server_username: str
+    server_password: str
+    server_token: str
+
+class UpdateTokenServerIP(LoginQuery):
+    tokenID: int
+    userID: int
+    server_ip: str
+
+class UpdateTokenServerPort(LoginQuery):
+    tokenID: int
+    userID: int
+    server_port: str
+
+class UpdateServerUsername(LoginQuery):
+    tokenID: int
+    userID: int
+    server_username: str
+
+class UpdateServerUserPassword(LoginQuery):
+    tokenID: int
+    userID: int
+    server_password: str
+
+class UpdateServerToken(LoginQuery):
+    tokenID: int
+    userID: int
+    server_token: str
+
+class SelectServerToken(LoginQuery):
+    tokenid: int
+    userid: int
+
+class DeleteServerToken(LoginQuery):
+    tokenid: int
+    userid: int
--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from settings import Settings
 from database import Database
 from datetime import datetime
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from integrations.integration_wolframalpha import *
 from integrations.integration_ai import IntegrationAi as ai
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET", "POST"],
+)
 ai.load_intents()
 
 def generate_token(data, type_data):
@@ -42,7 +48,7 @@ async def create_login_token(user: User):
     return generate_token(data_string, "logged_in_token")
 
 @app.post("/api/login/check_token_validity", tags=["login", "post"])
-async def login(login_query: Login_query):
+async def login(login_query: LoginQuery):
     connection_string = jwt.decode(login_query.login_query, key=Settings.get_encryption_key(), algorithms=[Settings.get_algorithm()])
     username = connection_string["username"]
     password = connection_string["password"]
@@ -54,82 +60,82 @@ async def login(login_query: Login_query):
     return generate_token(connection_string, "validated_token")
 
 @app.get("/api/{program_type}/check_command", tags=["windows", "discord", "get"])
-async def check_command(sentence, program_type):
-    return general_check_command(sentence, program_type)
+async def check_command(check_command: CheckQuery):
+    return general_check_command(check_command.sentence, check_command.program_type)
 
 @app.post("/api/user/create", tags=["user", "post"])
-async def create_new_user(username, password):
-    db_response = Database.create_new_user(username, password)
+async def create_new_user(new_user: NewUser):
+    db_response = Database.create_new_user(new_user.username, new_user.password)
     return JSONResponse(content=db_response, media_type="json")
 
 @app.put("/api/user/update/general", tags=["user", "put"])
-async def update_existing_user(userid, new_username, new_password):
-    db_response = Database.update_user(userid, new_username, new_password)
+async def update_existing_user(update_user_general: UpdateUserGeneral):
+    db_response = Database.update_user(update_user_general.userid, update_user_general.new_username, update_user_general.new_password)
     return JSONResponse(content=db_response, media_type="json")
 
 @app.patch("/api/user/update/username", tags=["user", "patch"])
-async def update_existing_user(userid, new_username):
-    db_response = Database.update_user(userid, new_username)
+async def update_existing_user(update_user_name: UpdateUserName):
+    db_response = Database.update_user(update_user_name.userid, update_user_name.new_username)
     return JSONResponse(content=db_response, media_type="json")
 
 @app.put("/api/user/update/password", tags=["user", "patch"])
-async def update_existing_user(userid, new_password):
-    db_response = Database.update_user(userid, new_password)
+async def update_existing_user(update_user_password: UpdateUserPassword):
+    db_response = Database.update_user(update_user_password.userid, update_user_password.new_password)
     return JSONResponse(content=db_response, media_type="json")
 
 @app.delete("/api/user/delete", tags=["user", "delete"])
-async def delete_existing_user(userid):
-    db_response = Database.delete_user(userid)
+async def delete_existing_user(delete_user: DeleteUser):
+    db_response = Database.delete_user(delete_user.userid)
     return JSONResponse(content=db_response, media_type="json")
 
 @app.post("/api/user/select", tags=["user", "post"])
-async def select_existing_user(program_type, username, password, discord_name="undefined"):
-    db_response = Database.select_user(program_type, username, password, discord_name)
+async def select_existing_user(select_user: SelectUser):
+    db_response = Database.select_user(select_user.program_type, select_user.username, select_user.password, select_user.discord_name)
     return JSONResponse(content=db_response, media_type="json")
 
 @app.post("/api/token/add", tags=["token", "post"])
-async def add_user_token(userID, server_ip, server_port, server_username, server_password, server_token):
-    Database.add_user_token(userID, server_ip, server_port, server_username, server_password, server_token)
+async def add_user_token(add_token: AddToken):
+    Database.add_user_token(add_token.userID, add_token.server_ip, add_token.server_port, add_token.server_username, add_token.server_password, add_token.server_token)
     return JSONResponse(content={"Result": "DONE"}, media_type="json")
 
 @app.put("/api/token/update/general", tags=["token", "put"])
-async def update_user_token(tokenID, userID, server_ip, server_port, server_username, server_password, server_token):
-    Database.update_user_token(tokenID, userID, server_ip, server_port, server_username, server_password, server_token)
+async def update_user_token(update_token: UpdateTokenGeneral):
+    Database.update_user_token(update_token.tokenID, update_token.userID, update_token.server_ip, update_token.server_port, update_token.server_username, update_token.server_password, update_token.server_token)
     return JSONResponse(content={"Result": "DONE"}, media_type="json")
 
 @app.patch("/api/token/update/server_ip", tags=["token", "patch"])
-async def update_user_token(tokenID, userID, server_ip):
-    Database.update_user_token(tokenID, userID, server_ip)
+async def update_user_token_server_ip(update_users_token_ip: UpdateTokenServerIP):
+    Database.update_user_token(update_users_token_ip.tokenID, update_users_token_ip.userID, update_users_token_ip.server_ip)
     return JSONResponse(content={"Result": "DONE"}, media_type="json")
 
 @app.put("/api/token/update/server_port", tags=["token", "patch"])
-async def update_user_token(tokenID, userID, server_port):
-    Database.update_user_token(tokenID, userID, server_port)
+async def update_user_token(update_users_token_port: UpdateTokenServerPort):
+    Database.update_user_token(update_users_token_port.tokenID, update_users_token_port.userID, update_users_token_port.server_port)
     return JSONResponse(content={"Result": "DONE"}, media_type="json")
 
 @app.put("/api/token/update/server_username", tags=["token", "patch"])
-async def update_user_token(tokenID, userID, server_username):
-    Database.update_user_token(tokenID, userID, server_username)
+async def update_user_token(update_server_username: UpdateServerUsername):
+    Database.update_user_token(update_server_username.tokenID, update_server_username.userID, update_server_username.server_username)
     return JSONResponse(content={"Result": "DONE"}, media_type="json")
 
 @app.put("/api/token/update/server_password", tags=["token", "patch"])
-async def update_user_token(tokenID, userID, server_password):
-    Database.update_user_token(tokenID, userID, server_password)
+async def update_user_token(update_server_user_password: UpdateUserPassword):
+    Database.update_user_token(update_server_user_password.tokenID, update_server_user_password.userID, update_server_user_password.server_password)
     return JSONResponse(content={"Result": "DONE"}, media_type="json")
 
 @app.put("/api/token/update/server_token", tags=["token", "patch"])
-async def update_user_token(tokenID, userID, server_token):
-    Database.update_user_token(tokenID, userID, server_token)
+async def update_user_token(update_server_token: UpdateServerToken):
+    Database.update_user_token(update_server_token.tokenID, update_server_token.userID, update_server_token.server_token)
     return JSONResponse(content={"Result": "DONE"}, media_type="json")
 
 @app.post("/api/token/select", tags=["token", "post"])
-async def select_user_token(tokenid, userid):
-    Database.select_user_token(tokenid, userid)
+async def select_user_token(select_server_token: SelectServerToken):
+    Database.select_user_token(select_server_token.tokenid, select_server_token.userid)
     return JSONResponse(content={"Result": "DONE"}, media_type="json")
 
 @app.post("/api/token/delete", tags=["token", "post"])
-async def delete_user_token(tokenid, userid):
-    Database.delete_user_token(tokenid, userid)
+async def delete_user_token(delete_server_token: DeleteServerToken):
+    Database.delete_user_token(DeleteServerToken.tokenid, DeleteServerToken.userid)
     return JSONResponse(content={"Result": "DONE"}, media_type="json")
 
 if __name__ == "__main__":
